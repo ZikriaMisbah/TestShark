3637c587b88a6185734a6f212c540d05dc795306
Zikria Misbah <zikriya.af@gmail.com>
2022-05-22 22:11:47 -0400

    Largest Number if Statement

---
+++ src/GitHubClass2.java
@@ -0,0 +1,18 @@
+
+public class GitHubClass2 {
+
+	public static void main(String[] args) {
+		int a = 500;
+		int b = 600;
+		int c = 700;
+
+		if (a > b && a > c) {
+			System.out.println(" a largest number");
+		} else if (b > c) {
+			System.out.println("b is largest number");
+		} else
+			System.out.println("c is largest number");
+
+	}
+
+}
