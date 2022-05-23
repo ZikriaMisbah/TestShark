175644d1de63547cc183983613d7cc308584ec66
Zikria Misbah <zikriya.af@gmail.com>
2022-05-22 20:36:11 -0400

    Changes In ArrayList

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -1,22 +1,34 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<String> cars = new ArrayList<String>();
-		
-cars.add("Mercedes");
-cars.add("BMW");
-cars.add("Audi");
-cars.add("Bogati");
-cars.remove(2);
-Collections.sort(cars);
-for (int i = 0; i<cars.size(); i++) {
-     System.out.println(cars.get(i));
-	
-}
+
+		cars.add("Mercedes");
+		cars.add("BMW");
+		cars.add("Audi");
+		cars.add("Bogati");
+		cars.remove(2);
+		Collections.sort(cars);
+		for (int i = 0; i < cars.size(); i++) {
+			System.out.println(cars.get(i));
+
+		}
+		    System.out.println("============");
+
+		ArrayList<Integer> MyNumber = new ArrayList<Integer>();
+		MyNumber.add(15);
+		MyNumber.add(17);
+		MyNumber.add(60);
+		MyNumber.add(10);
+		Collections.sort(MyNumber);
+		for (int i = 0; i < MyNumber.size(); i++) {
+			System.out.println(MyNumber.get(i));
+		}
 	}
 
 }
