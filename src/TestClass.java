12f185e5232943c00fab7d29afb4c6abd779f356
Zikria Misbah <zikriya.af@gmail.com>
2022-05-25 22:55:18 -0400

    new commit

---
+++ src/TestClass.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> number = new HashSet<Integer>();
+		number.add(4);
+		number.add(6);
+		number.add(9);
+		for(int i =0; i <=10; i++) {
+			if(number.contains(i)) {
+				System.out.println(i + " was found");
+			}else
+				System.out.println(i + " was not found");
+		}
+
+	}
+
+}
